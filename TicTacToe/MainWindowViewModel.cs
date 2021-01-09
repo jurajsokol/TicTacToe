748877cc124120914d7179f4ad543b1370a6dcaf
Juraj Sokol <jurajsokol.s@gmail.com>
2021-01-09 20:42:32 +0100

    Add project files.

---
+++ TicTacToe/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace TicTacToe
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ TicTacToe/TicTacToeGame.cs
@@ -0,0 +1,49 @@
+﻿using DynamicData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TicTacToe
+{
+    public enum Players
+    {
+        X,
+        O,
+    }
+
+    public class Field
+    { 
+        public int PositionX { get; }
+        public int PositionY { get; }
+        public Players PlayerType { get; }
+
+        public Field(int x, int y)
+        {
+            PositionX = x;
+            PositionY = y;
+        }
+    }
+
+    public class TicTacToeGame
+    {
+        private const int BOARD_SIZE = 4;
+        private SourceCache<Field, int> gameBoard;
+
+        public TicTacToeGame()
+        {
+            gameBoard = new SourceCache<Field, int>(f => f.PositionX * BOARD_SIZE + f.PositionY);
+
+            gameBoard.Edit(c => c.AddOrUpdate(
+                Enumerable.Range(0, BOARD_SIZE)
+                .Select(row => Enumerable.Range(0, BOARD_SIZE)
+                    .Select(column => new Field(column, row)))
+                .SelectMany(x => x)));
+        }
+
+        public IDisposable RegisterPlayerMoves(IObservable<Field> fieldChanges)
+        {
+            return fieldChanges.Subscribe(field => gameBoard.AddOrUpdate(field));
+        }
+    }
+}
